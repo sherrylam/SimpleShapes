437cbd984d8086fb1f3a245f4f2678cee1f00bb2
Sherry Lam <sherrylam2@gmail.com>
2021-02-04 01:15:33 -0800

    added simpleshapes files

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(600, 400);
+  print("hello hello")
+ }
+
+
+// Draw code goes here
+function draw() {
+  background('#E6E6FA');
+
+}
+
+function mousePressed() {
+
+	print("mouse pressed at x:" + mouseX + " y:" + mouseY);
+}
